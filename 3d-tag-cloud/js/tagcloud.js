1819144f6cf55e044d4fed7149af14164951a95d
Jorge Ktch <jorgektch@gmail.com>
2021-02-03 21:53:18 -0500

    3d tag cloud done

--- 3d-tag-cloud/js/tagcloud.js
+++ 3d-tag-cloud/js/tagcloud.js
@@ -1,23 +1,52 @@
-window.onload = function() {
-    try {
-        TagCanvas.Start('myCanvas','tags',{
-            textColour: '#ffffff',
-            outlineMethod: 'none',
-            
-            shape: "sphere",
-            textHeight: 30,
-            depth: 0.5,
+/*
+@font-face {
+    font-family: 'Ubuntu';
+    src: local('Ubuntu'),
+    local('Ubuntu Regular'),
+    local('Ubuntu-Regular'),
+    url('fonts/Ubuntu/Ubuntu-Regular.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'Ubuntu Mono';
+    src: local('Ubuntu Mono'),
+    local('UbuntuMono Regular'),
+    local('UbuntuMono-Regular'),
+    url('fonts/UbuntuMono/UbuntuMono-Regular.ttf') format('truetype');
+}
+*/
 
-            reverse: true,
-            dragControl: true,
-            
-            maxSpeed: 0.05,
-            
-            clickToFront: 500,
-            wheelZoom: false}
-        );
-    } catch(e) {
-      // something went wrong, hide the canvas container
-      document.getElementById('myCanvasContainer').style.display = 'none';
-    }
-  };
+window.onload = function() {
+  try {
+    var font = new FontFace(
+      "Ubuntu",
+      "url('fonts/Ubuntu/Ubuntu-Regular.ttf') format('truetype')"
+    );
+    
+    TagCanvas.Start('myCanvas','tags',{
+        textFont: 'Ubuntu',
+        textColour: '#ffffff',
+        outlineMethod: 'none',
+        
+        maxSpeed: 0.03,
+        depth: 0.5,
+        pulsateTo: 0.6,
+        initial: [0.1,-0.1],
+        decel: 0.95,
+        reverse: true,
+        fadeIn: 1000,
+        clickToFront: 600,
+
+        pulsateTime: 0.5,
+        wheelZoom: false,
+
+        shape: "sphere",
+        textHeight: 30,
+
+        zoom: 0.8
+        
+        }
+    );
+  } catch(e) {
+    document.getElementById('myCanvasContainer').style.display = 'none';
+  }
+};
